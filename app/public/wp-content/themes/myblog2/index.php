9bd9df97ce8e4d8703b735c85fdbc3aba0bb0812
hana00033 <ume_ideal@yahoo.co.jp>
2020-08-27 13:28:22 +0900

    header,index,single 修正

--- app/public/wp-content/themes/myblog2/index.php
+++ app/public/wp-content/themes/myblog2/index.php
@@ -11,26 +11,11 @@
 	<?php endif; ?>
  </div>
 
-<!-- <div class="bun">This page introduces all posts.</div>
-<div class="bun2">If you are interested in an article, you can click on it.
-</div> -->
 <div class="description">
 	<p>This page introduces all posts.</br>
 	If you are interested in an article, you can click on it.</p>
 </div>
 
-
-<!-- 	<ul class = "box">
-	<li><div class="box1"><img src="LVOL7703.jpeg"><p>タイに行った話</p></div></li>
-	<li><div class="box2"><img src="IMG_6188.JPG"><p>北海道に行った話</p></div></li>
-	<li><div class="box3"><img src="IMG_6829.JPG"><p>韓国に行った話</p></div></li>
- </ul>
-
- <ul class = "boxbox">
-	<li><div class="box4"><img src="IMG_0383.JPG"><p>シンガポールに行った話</p></div></li>
-	<li><div class="box5"><img src="BE8DA11F-D326-45E0-938F-CE8094EC72FE.jpeg"><p>NYに行った話</p></div></li>
-	<li><div class="box6"><img src="097FAFEB-795B-4905-8F85-B52201161B42.JPG"><p>香港に行った話</p></div></li>
- </ul> -->
 <div class="posts">
 
  <?php if (have_posts()): ?>
@@ -47,7 +32,7 @@
 					 <? endif; //条件分岐終了 ?>
 				 </a>
 	 </div>
-	 <h2 class="post_title"><?php the_title(); ?></h2>
+	 <a href="<?php the_permalink(); ?>"><h2 class="post_title"><?php the_title(); ?></h2></a>
 	 <div class="post_content">
 		 <p><?php the_time('Y.n.j.'); ?></p>
 		 <p class="post_tag_country"><?php the_category('&nbsp'); ?></p>
@@ -57,24 +42,6 @@
 <?php endwhile; ?>
 
 </div>
-
-
-
-<!-- <div class="pagination p1">
-  <ul>
-	<li href="#" class="triangle1"><a href="#"><span></span></a></li>
-	<li><a class="is-active" href="#">1</a></li>
-	<li><a class="is-active2"　href="#">2</a></li>
-	<li><a class="is-active2"　href="#">3</a></li>
-	<li href="#" class="curled-box"><a href="#"><span></span></a></li>
-	<li href="#" class="curled-box"><a href="#"><span></span></a></li>
-	<li href="#" class="curled-box"><a href="#"><span></span></a></li>
-	<li href="#" class="triangle2"><a href="#"><span></span></a></li>
-	
-  </ul>
-
-</div>
--->
 
  <?php the_posts_pagination(array('screen_reader_text'=>' ',
  'prev_next' => true,
@@ -93,13 +60,4 @@
 <?php get_footer(); ?>	
 	
 
-
-
-
-<!-- ▲ ヘッダー : 終了-->
-<!-- ▼ コンテンツ : 開始-->
-
-<!-- ▲ コンテンツ : 終了-->
-
-<!-- ▼ フッター : 開始-->
 
